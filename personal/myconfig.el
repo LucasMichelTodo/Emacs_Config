371d1975082c2112e3493928df00b067565f2afd
LucasMichelTodo <micheltod@gmail.com>
2019-08-05 11:27:42 +0200

    uncomment scrolling when sending a command.

--- personal/myconfig.el
+++ personal/myconfig.el
@@ -142,7 +142,7 @@
 
 ;; Command sending
 ;;----------------
-;;(setq comint-prompt-read-only t)
-;;(setq comint-scroll-to-bottom-on-input t)
-;;(setq comint-scroll-to-bottom-on-output t)
-;;(setq comint-move-point-for-output t)
+(setq comint-prompt-read-only t)
+(setq comint-scroll-to-bottom-on-input t)
+(setq comint-scroll-to-bottom-on-output t)
+(setq comint-move-point-for-output t)
